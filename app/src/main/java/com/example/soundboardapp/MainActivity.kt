e2bda65d64bd179b19cccf4305992b230b7d02fb
Tor B <torbrick@users.noreply.github.com>
2020-04-04 15:05:57 -0700

    Initial Commit

---
+++ app/src/main/java/com/example/soundboardapp/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.example.soundboardapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.soundboardapp.ui.main.TitleFragment
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                    .replace(R.id.container, TitleFragment.newInstance())
+                    .commitNow()
+        }
+    }
+}
