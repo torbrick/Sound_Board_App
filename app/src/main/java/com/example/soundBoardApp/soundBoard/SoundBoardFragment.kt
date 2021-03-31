f930e36808de4454b752427adc55ae29ebe57463
Tor B <torbrick@users.noreply.github.com>
2021-03-31 13:41:17 -0700

    Fab now opens a popup window that will be used for selection of image and sound to create new soundButton

--- app/src/main/java/com/example/soundBoardApp/soundBoard/SoundBoardFragment.kt
+++ app/src/main/java/com/example/soundBoardApp/soundBoard/SoundBoardFragment.kt
@@ -1,19 +1,32 @@
 package com.example.soundBoardApp.soundBoard
 
+import android.content.Context
+import android.content.Intent
+import android.graphics.Color
+import android.graphics.Color.rgb
+import android.graphics.drawable.ColorDrawable
+import android.graphics.drawable.Drawable
+import android.net.Uri
 import android.os.Bundle
+import android.view.Gravity
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.PopupWindow
+import android.widget.Toast
+import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.observe
 import androidx.recyclerview.widget.GridLayoutManager
+import com.example.soundBoardApp.R
 import com.example.soundBoardApp.databinding.FragmentSoundBoardBinding
 import com.example.soundBoardApp.tools.DependencyInjectors
 
 
 private const val TAG = "SBFragment"
 private const val NUM_SOUND_BUTTONS = 15
+private const val NUM_OF_BUTTON_COLUMNS = 3
 
 class SoundBoardFragment : Fragment() {
     /**
@@ -31,20 +44,19 @@ class SoundBoardFragment : Fragment() {
     private lateinit var soundBoardFragmentBinding : FragmentSoundBoardBinding
 
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
+        inflater: LayoutInflater,
+        container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        soundBoardFragmentBinding = FragmentSoundBoardBinding.inflate(inflater, container, false)
 
         val listAdapter = SoundButtonListAdapter() //create adapter
-        soundBoardFragmentBinding.buttonRecycler.adapter = listAdapter //bind adapter
-        
         subscribeUi(listAdapter)
 
-
-        val manager = GridLayoutManager(activity,3)
-        soundBoardFragmentBinding.buttonRecycler.layoutManager = manager
-
+        soundBoardFragmentBinding = FragmentSoundBoardBinding.inflate(inflater, container, false).apply {
+            soundBoardFragment = this@SoundBoardFragment //bind fragment for XML function calls
+            buttonRecycler.adapter = listAdapter //bind adapter
+            buttonRecycler.layoutManager = GridLayoutManager(activity,NUM_OF_BUTTON_COLUMNS)
+        }
 
         return soundBoardFragmentBinding.root
     }
@@ -62,4 +74,24 @@ class SoundBoardFragment : Fragment() {
                     }            
         }
     }
+
+
+    // TODO: 3/31/2021 define this in XML rather than programmatically
+    fun launchSoundButtonCreatorPopUp(view: View) {
+        Toast.makeText(view.context, "FAB Pressed", Toast.LENGTH_SHORT).show()
+        val sBCreatorPopUpWindowView = layoutInflater.inflate(R.layout.creator_sound_button, null)
+
+        val backGroundColor = ColorDrawable(Color.BLUE)
+
+        val windowWidth = ConstraintLayout.LayoutParams.WRAP_CONTENT
+        val windowHeight = ConstraintLayout.LayoutParams.WRAP_CONTENT
+        val sBCreatorPopUpWindow =
+            PopupWindow(sBCreatorPopUpWindowView, windowWidth, windowHeight).apply {
+                setBackgroundDrawable(backGroundColor)
+                showAtLocation(view, Gravity.CENTER, 0, 0)
+            }
+
+
+    }
+
 }
